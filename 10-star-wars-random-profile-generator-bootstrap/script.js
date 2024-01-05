5d26e3e437e0e9489fe4c635dceb38550259272c
deepak-parmar <25756259+deepak-parmar@users.noreply.github.com>
2024-01-05 16:26:11 +0530

    Module 10 bug fix
    - Update API URL
    - Render character without parsing

--- 10-star-wars-random-profile-generator-bootstrap/script.js
+++ 10-star-wars-random-profile-generator-bootstrap/script.js
@@ -1,48 +1,48 @@
 // Renders fetched character profile on page
 function renderCharacter(data) {
-	$("#name").html(`<h1>${data.name}</h1>`)
-	
-	$("#image").css({
-		background: `url(${data.image}) no-repeat top center`,
-		backgroundSize: "cover"
-	})
+  $("#name").html(`<h1>${data.name}</h1>`)
 
-	if (data.hasOwnProperty("died"))
-		$("#died").removeClass("d-none").addClass("d-flex")
-	else
-		$("#died").removeClass("d-flex").addClass("d-none")
+  $("#image").css({
+    background: `url(${data.image}) no-repeat top center`,
+    backgroundSize: "cover"
+  })
 
-	data.gender = data.gender == undefined ? "Unknown" : data.gender
-	$("#gender").text(data.gender)
-	
-	data.homeworld = data.homeworld == undefined ? "Unknown" : data.homeworld
-	$("#homeWorld").text(data.homeworld)
-	
-	data.species = data.species == undefined ? "Unknown" : data.species
-	$("#species").text(data.species)
+  if (data.hasOwnProperty("died"))
+    $("#died").removeClass("d-none").addClass("d-flex")
+  else
+    $("#died").removeClass("d-flex").addClass("d-none")
 
-	if (data.affiliations.length > 0) {
-		let affiliation = ''
-		
-		data.affiliations.forEach((element, index) => {
-			if (data.affiliations.length-1 == index) affiliation += `<span class="me-2">${element}</span>`
-			else affiliation += `<span class="me-2">${element},</span>`	
-		});
+  data.gender = data.gender == undefined ? "Unknown" : data.gender
+  $("#gender").text(data.gender)
 
-		$("#affiliations").html(affiliation)
-	} else {
-		$("#affiliations").text("None")
-	}
+  data.homeworld = data.homeworld == undefined ? "Unknown" : data.homeworld
+  $("#homeWorld").text(data.homeworld)
+
+  data.species = data.species == undefined ? "Unknown" : data.species
+  $("#species").text(data.species)
+
+  if (data.affiliations.length > 0) {
+    let affiliation = ''
+
+    data.affiliations.forEach((element, index) => {
+      if (data.affiliations.length - 1 == index) affiliation += `<span class="me-2">${element}</span>`
+      else affiliation += `<span class="me-2">${element},</span>`
+    });
+
+    $("#affiliations").html(affiliation)
+  } else {
+    $("#affiliations").text("None")
+  }
 }
 
 
 // Fetches character profile
 function fetchCharacter() {
-	$.get(
-		`https://raw.githubusercontent.com/akabab/starwars-api/0.2.1/api/id/${Math.ceil(Math.random() * 88)}.json`,
-		(data) => { renderCharacter(JSON.parse(data)) }
-	)
-	.fail(() => {alert("Character not found. Refresh!")})
+  $.get(
+    `https://akabab.github.io/starwars-api/api/id/${Math.ceil(Math.random() * 88)}.json`,
+    (data) => { renderCharacter(data) }
+  )
+    .fail(() => { alert("Character not found. Refresh!") })
 }
 
 
